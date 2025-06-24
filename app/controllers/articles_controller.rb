89b161f1b4a1310469b9c5d38dd552eba32425d3
Danilo Araujo <sa.danilo@gmail.com>
2025-06-24 11:10:52 -0300

    added editing route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,42 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "Article saved successifully"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article updated successifully"
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+  
+  def article_params
+    params.expect(article: [:title, :description])
+  end
+
+
 end
